8280179808043caac6733a649fb717d7b5cfb9be
jamesgawn <james@gawn.uk>
2021-07-24 11:48:25 +0100

    Add function versioning.

--- function.go
+++ function.go
@@ -2,18 +2,30 @@ package ddnsfunction
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func HandleDDNSUpdate(w http.ResponseWriter, r *http.Request) {
-	sendResponse(w, 200, "Hello World!")
+	SendResponse(w, 200, "Hello World!")
 }
 
-func sendResponse(w http.ResponseWriter, statusCode int, body string) {
+func SendResponse(w http.ResponseWriter, statusCode int, body string) {
+	version := ObtainVersion()
+	w.Header().Set("Version", version)
 	w.WriteHeader(statusCode)
+	w.Header().Set("Version", "0.0.0.0")
 	_, err := fmt.Fprint(w, body)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func ObtainVersion() string {
+	buf, err := ioutil.ReadFile("VERSION")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(buf)
 }
