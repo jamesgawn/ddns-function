93ec9328be8652fc1d02dba0358246164db155ab
jamesgawn <james@gawn.uk>
2021-07-25 17:48:38 +0100

    Add configuration to automated deployment updates

--- terraform.tf
+++ terraform.tf
@@ -190,3 +190,23 @@ resource "aws_lambda_permission" "permission" {
   principal = "apigateway.amazonaws.com"
   source_arn = "${aws_apigatewayv2_api.api.execution_arn}/*/*/*"
 }
+
+resource "aws_iam_group" "ddns-developer" {
+  name = "ddns-developers"
+}
+
+resource "aws_iam_group_policy" "deployer-policy"  {
+  name = "ddns-deployer-policy"
+  group = aws_iam_group.ddns-developer.name
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        Sid: "VisualEditor0",
+        Effect: "Allow",
+        Action: "lambda:UpdateFunctionCode",
+        Resource: "arn:aws:lambda:eu-west-2:979779020614:function:ddns"
+      }
+    ]
+  })
+}
